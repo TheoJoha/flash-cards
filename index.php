3fa2842d87acb32a382ef236573490960156900c
Theodor Johansson <t.j.u@telia.com>
2023-12-12 18:07:58 +0100

    fixed results bug

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $number = filter_input(INPUT_GET, "num", FILTER_SANITIZE_NUMBER_INT);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Multiplication Flash Cards</title>
-    <link rel="stylesheet" href="css/main.min.css" />
+    <link rel="stylesheet" href="css/main.css" />
 </head>
 <body>
     <?php include("./view/header.php"); ?>
--- view/results.php
+++ view/results.php
@@ -4,13 +4,13 @@
         for ($i = 1; $i <= 12; $i++) {
             $result = $number * $i;
             $cards .= "<div class='card' tabindex='0'
-                        aria-label={$number} multiplied by {$i}
+                        aria-label='{$number} multiplied by {$i}
                         equals {$result}'>
                             <div class='card-front'>
                             {$number} x {$i}
                             </div>
                             <div class='card-back'
-                            aria-label={$number} multiplied by {$i}
+                            aria-label='{$number} multiplied by {$i}
                             equals {$result}'>
                             {$result}
                             </div>
